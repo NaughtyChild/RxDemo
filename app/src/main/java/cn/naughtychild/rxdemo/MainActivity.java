40341a4165e567551f8213233d30a58a5629e717
NaughtyChild <1050762704@qq.com>
2020-04-27 16:20:24 +0800

    增加判断实例

--- app/src/main/java/cn/naughtychild/rxdemo/MainActivity.java
+++ app/src/main/java/cn/naughtychild/rxdemo/MainActivity.java
@@ -27,14 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                combineLastTest();
-            }
-        }.start();
-        PublishSubject<String> subject=PublishSubject.create();
+        all();
     }
 
     private void interval() {
@@ -249,6 +242,105 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void call(String s) {
                 Log.d("MainActivity", "call: " + s);
+            }
+        });
+    }
+
+    private void delay() {
+        Observable.just(1, 2, 3, 4, 5, 6).delay(6, TimeUnit.SECONDS).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.d("MainActivity", "call: " + integer);
+            }
+        });
+    }
+
+    private void timeOut() {
+        Observable.just(1, 2, 3, 4, 5).doOnNext(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                try {
+                    Thread.sleep(integer * 100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).timeout(300, TimeUnit.MILLISECONDS, Observable.just(10, 11, 12)).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.d("MainActivity", "call: " + integer);
+            }
+        });
+    }
+
+    private void onErrorReturn() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 5; i++) {
+                    subscriber.onNext(i);
+                    if (i == 4) {
+                        Log.d("MainActivity", "call: 发生错误了");
+                        subscriber.onError(new Throwable("yuejie"));
+                    }
+                }
+            }
+        }).onErrorReturn(new Func1<Throwable, Integer>() {
+            @Override
+            public Integer call(Throwable throwable) {
+                return 5;
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.d("MainActivity", "call: " + integer);
+            }
+        });
+    }
+
+    private void retryWhen() {
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 4; i++) {
+                    subscriber.onNext(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if (i == 2) {
+                        subscriber.onError(new Throwable("错误了"));
+                    }
+                }
+            }
+        }).retry(2).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("MainActivity", "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d("MainActivity", "onNext: " + integer);
+            }
+        });
+    }
+
+    private void all() {
+        Observable.just(1, 2, 3, 4, 5, 6).all(new Func1<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) {
+                return integer > 3;
+            }
+        }).subscribe(new Action1<Boolean>() {
+            @Override
+            public void call(Boolean aBoolean) {
+                Log.d("MainActivity", "call: " + aBoolean);
             }
         });
     }
